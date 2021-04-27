4e8f89c1870297d69e9cf15eb9501823d6d9f11e
Paula Rodríguez <37958437+pawrdgz@users.noreply.github.com>
2021-04-27 12:51:50 +0200

    menu arreglado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
+// Inicializar el fade-in 
+
+AOS.init();
+
+
+// Carrusel 
+
+new Splide('.splide', {
+    type: 'loop',
+    autoWidth: true,
+    gap: '8.4rem',
+    focus: 'center'
+    }).mount();
 
 
 // Menu open
@@ -16,7 +29,7 @@ $("#arrowtop").on("click", function() {
     $("body,html").animate({
         scrollTop: $("#projects").position().top
     }, 1300)
-})
+    })
 
 // Flecha que dirige hacia top     
 
@@ -24,95 +37,16 @@ $("#arrowbottom").on("click", function() {
     $("body,html").animate({
         scrollTop: 0
     }, 1250)
-})
-
-// Inicializar el fade-in 
-
-AOS.init();
-
-
-// Carrusel 
-
-new Splide('.splide', {
-    type: 'loop',
-    autoWidth: true,
-    gap: '8.4rem',
-    focus: 'center'
-}).mount();
-
-
-
-
-    });
-
-
-
-
-
-
-// Intento de menú sticky
-
-$(document).ready(function() {
-
-    // Dock the header to the top of the window when scrolled past the banner.
-    // This is the default behavior.
-
-    $('.menu').scrollToFixed( 
-        {
-            marginTop:500
-        }
-     );
-
-});
-
-
-// detectar dónde está el menú
-
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll <2000) {
-        $(".nav-item-projects").addClass("menu-is-active");
-    }  
-    
-    else {
-        $(".nav-item-projects").removeClass("menu-is-active");
-    } 
-
-});
-
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if ((scroll >=2000) && (scroll <=3000)) {
-        $(".nav-item-about").addClass("menu-is-active");
-    }  
-
-    else {
-        $(".nav-item-about").removeClass("menu-is-active");
-    }
-});
-
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >3000){
-        $(".nav-item-contact").addClass("menu-is-active");
-    }  
-    
-    else {
-        $(".nav-item-contact").removeClass("menu-is-active");
-    } 
-});
+    })
 
 // typed
 
-var typed = new Typed('.billboard_title_invert', {
+new Typed('.billboard_title_invert', {
     strings: ['Tiramisu enjoyer','The Batman', 'UX Designer'],
     typeSpeed: 50,
     backSpeed: 40,
     smartBackspace: true,
-  });
+    });
 
 
 // smooth movement entre a en one page
@@ -120,5 +54,34 @@ var typed = new Typed('.billboard_title_invert', {
   $('a[href*=\\#]').on('click', function(event){     
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+    });
+
+
+    });
+
+
+// detectar dónde está el menú
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var projectsTop = $("#projects").position().top;
+    var aboutTop = $("#about").position().top;
+    var contactTop = $("#contact").position().top;
+
+    if (scroll >= projectsTop && scroll < aboutTop) {
+        $(".nav-item").removeClass("menu-is-active");
+        $(".nav-item-projects").addClass("menu-is-active");
+    }   else if (scroll >= aboutTop && scroll < contactTop) {
+        $(".nav-item").removeClass("menu-is-active");
+        $(".nav-item-about").addClass("menu-is-active");
+    }   else if (scroll >= contactTop) {
+        $(".nav-item").removeClass("menu-is-active");
+        $(".nav-item-contact").addClass("menu-is-active");
+    }   else {
+        $(".nav-item").removeClass("menu-is-active");
+    }
+
 });
+
+
 
